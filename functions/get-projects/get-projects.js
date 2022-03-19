4a11a7b57752c5cacde341d88a0f77215daf578c
Raymond Camden <raymondcamden@gmail.com>
2022-03-19 10:55:13 -0500

    minor layout

--- functions/get-projects/get-projects.js
+++ functions/get-projects/get-projects.js
@@ -42,10 +42,9 @@ const handler = async (event, context) => {
 	}));
 
 	return {
-    statusCode:200,
-    body:JSON.stringify(data)
-  };
-
+		statusCode:200,
+		body:JSON.stringify(data)
+	};
 
 }
 
